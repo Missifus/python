9e4da7198e649292d168c0cd7b0a371029bd2f4c
David Diaz <david2002ecua@gmail.com>
2024-07-29 19:41:22 -0500

    first commit

---
+++ holaMundo.py
@@ -0,0 +1,6 @@
+#variables
+nombre = "David Diaz"
+edad = 21
+print ("Hola, me presento mi nombre es ",nombre," y tengo",str(edad),"años. Y dentro de 10 años tendre ",str(edad+10))
+print (2 ** 3)
+print (8 / 3)
